56864af82cacb07a56b43b9324ea794db49531ca
Pruthviraj Rathod <pruthvirajrathod645@gmail.com>
2026-02-21 00:39:22 +0530

    Initial portfolio project commit

---
+++ src/constants.js
@@ -0,0 +1,255 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import ambLogo from './assets/company_logo/amb.png';
+import codeAlphaLogo from './assets/company_logo/codeAlpha.png';
+import amdoxLogo from './assets/company_logo/amdox.png';
+
+// Education Section Logo's
+import icemLogo from './assets/education_logo/icem.png';
+import gpbLogo from './assets/education_logo/gpb.png';
+import nesLogo from './assets/education_logo/nes.png';
+
+// Project Section Logo's
+import EcobinLogo from './assets/work_logo/Ecobin.png';
+// import DesktopaLogo from './assets/work_logo/Desktopa.png';
+import desktopa from "./assets/work_logo/desktopa.png";
+// import movierecLogo from './assets/work_logo/movie_rec.png';
+// import taskremLogo from './assets/work_logo/task_rem.png';
+// import npmLogo from './assets/work_logo/npm.png';
+import portfolioLogo from './assets/work_logo/portfolio.png';
+import artiLogo from './assets/work_logo/arti.png';
+import codesLogo from './assets/work_logo/codes.png';
+// import webverLogo from './assets/work_logo/web_dig.png';
+// import cmLogo from './assets/work_logo/cm.png';
+// import imagesearchLogo from './assets/work_logo/image_search.png';
+// import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      // { name: 'Redux', logo: reduxLogo },
+      // { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      // { name: 'Node JS', logo: nodejsLogo },
+      // { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      // { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      // { name: 'Postman', logo: postmanLogo },
+      // { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      // { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: ambLogo,
+      role: "Industrial Training",
+      company: "Amb's C-DSP Pvt. Ltd",
+      date: "June – July 2023 (6 Weeks) ",
+      desc: " Gained practical experience in Python programming and IoT concepts. \n• Developed basic Python projects, enhancing data-driven application development skills.",
+      skills: [
+        "Python",
+        "Embeedded Systems",
+        // "JavaScript",
+        // "React JS",
+        // "TypeScript",
+        // "Node JS",
+        // "Tailwind CSS",
+        // "MongoDb",
+        // "Redux",
+        // " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: codeAlphaLogo,
+      role: "Java Intern",
+      company: "CODE ALPHA",
+      date: "May – June 2025 (2 Months) ",
+      desc: " Worked on Java backend development and contributed to collaborative project implementation. \n• Gained hands-on experience in object-oriented programming and software development practices.",
+      skills: [
+        "Java",
+        "MySql",
+        // "JavaScript",
+        // "Tailwind CSS",
+        // "HTML",
+        // "CSS",
+        // "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: amdoxLogo,
+      role: "Java Full stack Intern",
+      company: "Amdox Technologies",
+      date: "09 Jan - Present ",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "React JS",
+        "Java",
+        "Spring Boot",
+        "MySql",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: icemLogo,
+      school: "Indira College of Engineering and Management, Pune.",
+      date: "2024- 2027 ",
+      grade: "Pursuing in 3rd year",
+      // desc: ".",
+      degree: "Bachelor of Engineering (Computer Engineering)",
+    },
+    {
+      id: 1,
+      img: gpbLogo,
+      school: "Government Polytechnic, Beed",
+      date: "2021- 2024",
+      grade: "82.46%",
+      // desc: ".",
+      degree: "Diploma (Computer Technology)",
+    },
+    {
+      id: 2,
+      img: nesLogo,
+      school: "New English School, Doddi Solapur",
+      date: "2020- 2021 ",
+      grade: "84.40%",
+      // desc: "",
+      degree: "Secondary School Certificate",
+    },
+  
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "EcoBin Application",
+      description:
+        "Contributed to the development of a smart waste management mobile application promoting responsible disposal, citizen participation, and data-driven cleanliness solutions. Implemented the front-end using Jetpack Compose to create an intuitive and responsive user interface for users and administrators.Integrated Firebase Authentication (OTP-based login) and Firestore Database for secure user data storage and real-time updates.Designed and developed modules for waste reporting, location-based tracking, and analytics dashboards, enhancing transparency and efficiency in waste collection..",
+      image: EcobinLogo,
+      tags: ["Figma", "Firebase", "Kotlin", "Jetpack Compose", "API"],
+      github: "https://github.com/EARTHKING12",
+      webapp: "https://github.com/EARTHKING12",
+    },
+    {
+      id: 1,
+      title: "Desktop Assistant",
+      description:
+        "Developed a Python-based voice-controlled virtual assistant for automating daily tasks and enhancing productivity.Utilized SpeechRecognition, pyttsx3, and automation libraries to enable natural voice interaction and task execution.Implemented features such as opening applications, web searching, system control, and voice feedback.Demonstrated strong understanding of Python scripting, API integration, and event-driven programming.",
+      image: desktopa,
+      tags: ["Python", "Voice Recognition", "Text-to-Speech", "Task Automation"],
+      github: "https://github.com/EARTHKING12",
+      webapp: "https://github.com/EARTHKING12/",
+    },
+    {
+      id: 2,
+      title: "Artisan Handicraft –E-commerce Website",
+      description:
+        "Developed a responsive e-commerce platform to showcase and sell handcrafted products with an engaging and user-friendly interface.Built the front-end using HTML, CSS, and JavaScript, ensuring responsiveness and smooth navigation across devices.Implemented product listing, search, and cart functionalities to enhance the shopping experience.Focused on UI/UX design for improved aesthetics and user engagement, highlighting traditional craftsmanship through modern web design.",
+      image: artiLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "https://github.com/EARTHKING12",
+      webapp: "https://github.com/EARTHKING12",
+    },
+    {
+      id: 3,
+      title: "My Portfolio Website",
+      description:
+        "A responsive and modern personal portfolio website designed to showcase my skills, projects, and experience as a full-stack developer. The website highlights my technical expertise, project work, and contact information in a clean and user-friendly interface.",
+      image: portfolioLogo,
+      tags: ["React JS", "Tailwind CSS", "JavaScript", "HTML", "CSS"],
+      github: "https://github.com/EARTHKING12/Pruthvi-r-Portfolio",
+      webapp: "https://pruthvirajrathod.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "CodeSpringer ",
+      description:
+        " Professional Digital Services for Your Brand",
+      image: codesLogo,
+      tags: ["HTML", "CSS", "JavaScript",],
+      github: "https://github.com/EARTHKING12/CodeSpringer?tab=readme-ov-file",
+      webapp: "https://earthking12.github.io/CodeSpringer/",
+    },
+  ];  
